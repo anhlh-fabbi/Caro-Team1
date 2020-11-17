043aa0b4ccbcf990e7fe6f1671b7145c602d5299
anhlh <anhlh@fabbi.io>
2020-11-17 23:25:20 +0700

    create bot 100%

--- Caro.py
+++ Caro.py
@@ -4,13 +4,21 @@ import time
 import Ve
 
 count = 0  # chan x danh, le O danh
+ChienThang = False
 dtx = [0, 1, 1, 1]
 dty = [1, 0, 1, -1]
 
+def KTChienThang(x, y, type):
+    global board, ChienThang
+    listDiem, listChienThang = luuListDiem(x, y,type)
+    if ttChienThang(listChienThang) == 5:
+        ChienThang = True
+        return True
+    return False
 
 def chuyenDoiDiem(listDiem):
     # print(listDiem)
-    tungdiem = {0: {},1: {},2: {},3: {},4: {},5: {},-1: {}}
+    tungdiem = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, -1: {}}
     for key in listDiem:
         for diem in listDiem[key]:
             if key in tungdiem[diem]:
@@ -18,13 +26,16 @@ def chuyenDoiDiem(listDiem):
             else:
                 tungdiem[diem][key] = 1
     return tungdiem
+
+
 def diem34(diem3, diem4):
     for key4 in diem4:
-        if diem4[key4] >=1:
+        if diem4[key4] >= 1:
             for key3 in diem3:
-                if key3 != key4 and diem3[key3] >=2:
-                        return True
+                if key3 != key4 and diem3[key3] >= 2:
+                    return True
     return False
+
 
 def ttChienThang(listDiem):
     listDiem = chuyenDoiDiem(listDiem)
@@ -35,10 +46,11 @@ def ttChienThang(listDiem):
     elif diem34(listDiem[3], listDiem[4]):
         return 4
     else:
-        diem3 =sorted(listDiem[3].values(), reverse=True)
+        diem3 = sorted(listDiem[3].values(), reverse=True)
         if len(diem3) >= 2 and diem3[0] >= diem3[1] >= 2:
             return 3
     return 0
+
 
 def banCoRong():
     global size
@@ -113,11 +125,11 @@ def tinhDiem(arr, type):
 def luuListDiem(x, y, type):
     global dtx, dty
     arr = {-1: 0, 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
-    Diem = {(0,1): [], (1,0): [], (1,1): [], (1,-1): []}
+    Diem = {(0, 1): [], (1, 0): [], (1, 1): [], (1, -1): []}
     for i in range(len(dtx)):
         MangDT = chuyenDuongThangRaMangGiaTri(x, y, dtx[i], dty[i])
         listDiem = tinhDiem(MangDT, type)
-        Diem[(dtx[i], dty[i])]= listDiem
+        Diem[(dtx[i], dty[i])] = listDiem
         for i in arr:
             arr[i] += listDiem.count(i)
     return arr, Diem
@@ -127,32 +139,37 @@ def DiemNuocDi(x, y):
     tc = 0
     pt = 0
     board[x][y] = 'o'
-    listDiem, listChienThang  = luuListDiem(x,y,'o')
+    listDiem, listChienThang = luuListDiem(x, y, 'o')
     board[x][y] = ' '
-    tc += ttChienThang(listChienThang)*5000 + listDiem[-1] + listDiem[1] + 4*listDiem[2] + 8*listDiem[3] + 16*listDiem[4]
+    tc += ttChienThang(listChienThang) * 5000 + listDiem[-1] + listDiem[1] + 4 * listDiem[2] + 8 * listDiem[3] + 16 * \
+          listDiem[4]
     board[x][y] = 'x'
-    listDiem, listChienThang = luuListDiem(x,y,'x')
+    listDiem, listChienThang = luuListDiem(x, y, 'x')
     # print (str(x) + " " + str(y))
     # print (listDiem)
     # print ('\n')
-    pt += ttChienThang(listChienThang)*4500 + listDiem[-1] + listDiem[1] + 4 * listDiem[2] + 8 * listDiem[3] + 16 * listDiem[4]
+    pt += ttChienThang(listChienThang) * 4500 + listDiem[-1] + listDiem[1] + 4 * listDiem[2] + 8 * listDiem[3] + 16 * \
+          listDiem[4]
     board[x][y] = ' '
-    print(str(x) + " " + str(y) + ": " + str(tc+pt))
+    print(str(x) + " " + str(y) + ": " + str(tc + pt))
     return tc + pt
+
 
 def nuocDiTotNhat():
     global colors, board
     listToaDo = toaDoCoTheDanh()
     max = 0
-    (x,y) = (0,0)
+    (x, y) = (0, 0)
     for (_x, _y) in listToaDo:
         Diem = DiemNuocDi(_x, _y)
-        if(Diem > max):
+        if (Diem > max):
             max = Diem
             (x, y) = (_x, _y)
     board[x][y] = 'o'
     print("\n\n")
-    Ve.O(x,y, colors)
+    Ve.O(x, y, colors)
+    return x,y
+
 
 def toaDoCoTheDanh():
     global board, dtx, dty, size
@@ -172,21 +189,21 @@ def toaDoCoTheDanh():
     return listToaDoTrong
 
 
-
-
-
-
-
 def click(x, y):
     x = int(x)
     y = int(y)
-    global colors
+    global colors, ChienThang
 
     global size, count, board
-    if 0 <= x < size and 0 <= y < size and board[x][y] == ' ':
+    if 0 <= x < size and 0 <= y < size and board[x][y] == ' ' and ChienThang == False:
         Ve.X(x, y, colors)
         board[x][y] = 'x'
-        nuocDiTotNhat()
+        if(KTChienThang(x, y, 'x') == True):
+            print('Nguoi Thang')
+        else:
+            x, y = nuocDiTotNhat()
+            if(KTChienThang(x,y, 'o') == True):
+                print('May Thang')
         # print("\n\n")
 
 
